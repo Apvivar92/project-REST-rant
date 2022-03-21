e4f14540eab63b5880aab01407fd19b2aaa9839c
Paul  Vivar <Apvivar92@gmail.com>
2022-03-21 15:04:14 -0700

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ app.get('/', (req, res) => {
   res.send("Homepage")
 });
 
-// make wild card route path 
+// make wild card route path. (invalid address)
 // (make sure it's place at bottom otherwise page will be override other pages)
 app.get('*', (req, res) => {
   // .status(404) linked to .send to call status response
+  // .send sends message to page
   res.status(404).send('<h1>404 Page</h1>')
 })
 
