44839f9ab9963514330359fa0e10a3c0dd57e8c5
Paul  Vivar <Apvivar92@gmail.com>
2022-03-21 13:12:30 -0700

    Added 404 page route

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 
+// homepage route path
 app.get('/', (req, res) => {
   res.send("Hello World")
 });
 
-app.listen(5000,() => {
-  console.log("Server on!")
+// make wild card route path 
+// (make sure it's place at bottom otherwise page will be override other pages)
+app.get('*', (req, res) => {
+  // .status(404) linked to .send to call status response
+  res.status(404).send('<h1>404 Page</h1>')
 })
+
+app.listen(process.env.PORT)
